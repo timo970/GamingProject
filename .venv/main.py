e0726aca192856dadd425ea02d582399b84bd466
Костя <belskiy.konstantin@gmail.com>
2026-01-19 00:42:04 +0300

    options added

--- .venv/main.py
+++ .venv/main.py
@@ -1,13 +1,146 @@
 import arcade as ar
-from arcade.gui import UIManager, UIFlatButton, UILabel
+from PIL import Image, ImageDraw
+from arcade import View
+from arcade.gui import UIManager, UIFlatButton, UILabel, UIDropdown, UISlider, UITextureButton
+from pyglet.event import EVENT_HANDLE_STATE
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+VOLUME = 100
+FULL_SCREEN = False
+
+
+class OptionsScene(ar.View):
+    def __init__(self, window):
+        super().__init__(window, ar.color.BLUE_SAPPHIRE) # Поменять цвет на фон
+        self.window = window
+        self.manager = UIManager()
+        self.manager.enable()
+        scale = window.width / 800
+        options_label = UILabel(
+            text='Настройки',
+            x=200 * scale,
+            y=459 * scale,
+            height=81 * scale,
+            width=400 * scale,
+            font_size=30 * scale,
+            align='center',
+            text_color=ar.color.WHITE
+        )
+        self.volume_label = UILabel(
+            text='Громкость звука',
+            x=160 * scale,
+            y=450 * scale,
+            height=41 * scale,
+            width=171 * scale,
+            font_size=20 * scale,
+            align='left',
+            text_color=ar.color.WHITE
+        )
+        resolution_label = UILabel(
+            text='Разрешение',
+            x=160 * scale,
+            y=360 * scale,
+            height=41 * scale,
+            width=171 * scale,
+            font_size=20 * scale,
+            align='left',
+            text_color=ar.color.WHITE
+        )
+        full_screen_label = UILabel(
+            text='Полный экран',
+            x=160 * scale,
+            y=270 * scale,
+            height=41 * scale,
+            width=171 * scale,
+            font_size=20 * scale,
+            align='left',
+            text_color=ar.color.WHITE
+        )
+        slider_label = UILabel(
+            text='100',
+            x=544 * scale,
+            y=460 * scale,
+            height=21 * scale,
+            width=16 * scale,
+            font_size=10 * scale,
+            align='left',
+            text_color=ar.color.WHITE
+        )
+        self.slider = UISlider(
+            value=100,
+            x=464 * scale,
+            y=430 * scale,
+            width=171 * scale,
+            height=22 * scale,
+            min_value=0,
+            max_value=100,
+            step=1
+        )
+        self.resolution_button = UIDropdown(
+            x=464 * scale,
+            y=430 * scale,
+            width=171 * scale,
+            height=22 * scale,
+            options=[
+                '800:600',
+                '1600:900',
+                '1920:1080',
+                '2560:1440',
+                '3840:2160'
+            ],
+            default='800:600'
+        )
+        self.full_screen_button = UIDropdown(
+            x=464 * scale,
+            y=270 * scale,
+            width=20 * scale,
+            height=20 * scale,
+            options=[
+                'Да',
+                'Нет'
+            ],
+            default='Нет'
+        )
+        self.slider.on_change = self.volume_change
+        self.resolution_button.on_change = self.resolution_change
+        self.full_screen_button.on_change = self.full_screen_change
+        self.manager.add(options_label)
+        self.manager.add(slider_label)
+        self.manager.add(resolution_label)
+        self.manager.add(full_screen_label)
+        self.manager.add(self.slider)
+        self.manager.add(self.full_screen_button)
+        self.manager.add(self.volume_label)
+        self.manager.add(self.resolution_button)
+
+    def on_draw(self) -> bool | None:
+        self.clear()
+        self.manager.draw()
+        return
+
+    def on_key_press(self, symbol: int, modifiers: int) -> bool | None:
+        if symbol == ar.key.ESCAPE:
+            self.window.show_view(self.window.sub_view)
+        return
+
+    def volume_change(self, event):
+        global VOLUME
+        VOLUME = self.slider.value
+        self.volume_label.text = str(self.slider.value)
+
+    def resolution_change(self, event):
+        global SCREEN_HEIGHT, SCREEN_WIDTH
+        SCREEN_WIDTH, SCREEN_HEIGHT = [int(i) for i in self.resolution_button.value.split(':')]
+        self.window.on_resize(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    def full_screen_change(self, event):
+        self.window.set_fullscreen(self.full_screen_button.value == 'ДА')
 
 
 class FirstScene(ar.View):
     def __init__(self, window):
-        super().__init__(window, ar.color.BLUE_SAPPHIRE)
+        super().__init__(window, ar.color.BLUE_SAPPHIRE) # Поменять цвет на фон
         self.manager = UIManager()
         self.manager.enable()
         main_menu_label = UILabel(
@@ -19,7 +152,6 @@ class FirstScene(ar.View):
             x=200,
             y=520
         )
-        self.manager.add(main_menu_label)
         play_button = UIFlatButton(
             x=285,
             y=360,
@@ -27,7 +159,6 @@ class FirstScene(ar.View):
             width=230,
             height=31
         )
-        self.manager.add(play_button)
         options_button = UIFlatButton(
             x=285,
             y=300,
@@ -35,7 +166,6 @@ class FirstScene(ar.View):
             width=230,
             height=31
         )
-        self.manager.add(options_button)
         autors_button = UIFlatButton(
             x=285,
             y=240,
@@ -43,7 +173,6 @@ class FirstScene(ar.View):
             width=230,
             height=31
         )
-        self.manager.add(autors_button)
         exit_button = UIFlatButton(
             x=285,
             y=180,
@@ -52,6 +181,11 @@ class FirstScene(ar.View):
             height=31
         )
         exit_button.on_click = self.exit
+        options_button.on_click = self.options
+        self.manager.add(main_menu_label)
+        self.manager.add(play_button)
+        self.manager.add(options_button)
+        self.manager.add(autors_button)
         self.manager.add(exit_button)
 
     def on_draw(self) -> bool | None:
@@ -62,12 +196,21 @@ class FirstScene(ar.View):
     def exit(self, event):
         self.window.close()
 
+    def options(self, event):
+        self.window.show_view_new(self.window.options_view)
+
 
 class Game(ar.Window):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, 'Game', )  # Потом заменить имя приложения
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, 'Game')  # Потом заменить имя приложения
         self.first_scene = FirstScene(self)
-        self.show_view(self.first_scene)
+        self.options_view = OptionsScene(self)
+        self.show_view_new(self.first_scene)
+        self.sub_view = self.first_scene
+
+    def show_view_new(self, new_view: View) -> None:
+        self.sub_view = self.view
+        self.show_view(new_view)
 
 
 game = Game()
